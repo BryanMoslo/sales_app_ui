fa310eab063d96df87186107fd63dd07cbd571f3
Danier Martinez <dmartinez@wawand.co>
2022-12-15 15:41:12 -0500

    SA-15 create a client

---
+++ src/components/clients/Create.js
@@ -0,0 +1,83 @@
+import Form from "../common/form";
+import {useEffect, useState} from "react";
+import data from "bootstrap/js/src/dom/data";
+import {redirect, useNavigate} from "react-router-dom";
+
+const baseUrl = `http://localhost:3000/clients`
+
+
+export default function Create() {
+    const initialForm = {name: '', phone: '', rep: ''}
+    const [client, setClient] = useState(initialForm)
+    const [isSubmitted, setIsSubmitted] = useState(false)
+    const navigate = useNavigate()
+
+
+    useEffect(() => {
+        if(isSubmitted) {
+            const sanitizedNumber = client.phone.replaceAll('-', '')
+
+            const options = {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({
+                    name: client.name,
+                    phone_number: sanitizedNumber,
+                    rep: client.rep
+                })
+            };
+
+            fetch(`${baseUrl}/create`, options)
+                .then(res => {
+                    if (res.ok) {
+                        navigate('/clients')
+                    }
+                    setIsSubmitted(false)
+                })
+                .catch(err => console.error(err))
+        }
+
+
+    }, [isSubmitted]);
+    
+
+    function handleInputChange(e) {
+        const { target } = e
+        const { name, value } = target
+        setClient({...client, [name]: value})
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault()
+
+        setIsSubmitted(true)
+    }
+
+
+    return (
+        <Form title="Create a client" onSubmit={handleSubmit} >
+            <div className="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
+                <div>
+                    <label className="text-gray-700" htmlFor="name">Name</label>
+                    <input className="w-full mt-2 h-11 border-gray-200 rounded-md focus:border-indigo-600 focus:ring focus:ring-opacity-40 focus:ring-indigo-500" type="text" name="name" value={client.name} placeholder="Client Name" onChange={handleInputChange}/>
+                </div>
+
+                <div>
+                    <label className="text-gray-700" htmlFor="name">Phone Number</label>
+                    <input
+                        className="w-full mt-2 h-11 border-gray-200 rounded-md focus:border-indigo-600 focus:ring focus:ring-opacity-40 focus:ring-indigo-500"
+                        type="text"
+                        name="phone"
+                        value={client.phone}
+                        placeholder="000-000-0000"
+                        onChange={handleInputChange}/>
+                </div>
+
+                <div>
+                    <label className="text-gray-700" htmlFor="name">Rep</label>
+                    <input className="w-full mt-2 h-11 border-gray-200 rounded-md focus:border-indigo-600 focus:ring focus:ring-opacity-40 focus:ring-indigo-500" type="text" name="rep" value={client.representative} placeholder="Rep" onChange={handleInputChange}/>
+                </div>
+            </div>
+        </Form>
+    )
+}
--- src/components/teams/List.js
+++ src/components/teams/List.js
@@ -1,6 +1,7 @@
 import Table from "../common/table";
 import {useState, useEffect} from "react";
 import {Link } from "react-router-dom";
+import List from "../common/list";
 
 function Teams() {
     const baseUrl = 'http://localhost:3000/teams/';
@@ -39,6 +40,7 @@ function Teams() {
                         setCurrentDeleted('')
                     }
                 })
+                .catch(err => console.error(err))
         }
 
         return () => {
@@ -73,52 +75,39 @@ function Teams() {
     }
 
     return (
-    <div>
-        <div className="mt-4 flex justify-between">
-            <h3 className="text-3xl font-medium text-gray-700">Teams</h3>
+        <List isLoading={isLoading} title="Team" linkTo="/teams/create">
+            <Table columns={['#','Name', 'Industry']}>
+                <>
+                    {teams.map((team, i) => (
+                        <tr key={team.id} className={team.id === currentDeleted ? deletingClass : ''}>
+                            <td className="px-6 py-4 border-b border-gray-200 whitespace-nowrap">
+                                <div className="text-sm leading-5 text-gray-900">
+                                    <Link to={team.id}>{i+1}</Link>
+                                </div>
+                            </td>
 
-            <Link to="/teams/create">
-                <button className="px-4 py-2 font-medium tracking-wide text-white transition-colors duration-200 transform bg-indigo-600 rounded-md hover:bg-indigo-500 focus:outline-none focus:bg-indigo-500">
-                    Create a Team
-                </button>
-            </Link>
-        </div>
+                            <td className="px-6 py-4 border-b border-gray-200 whitespace-nowrap">
+                                <div className="text-sm leading-5 text-gray-900">
+                                    {team?.name ? team?.name : '-'}
+                                </div>
+                            </td>
 
-
-        {isLoading ? (
-            <div>Loading...</div>
-        ) : <Table columns={['#','Name', 'Industry']}>
-            <>
-                {teams.map((team, i) => (
-                    <tr key={team.id} className={team.id === currentDeleted ? deletingClass : ''}>
-                        <td className="px-6 py-4 border-b border-gray-200 whitespace-nowrap">
-                            <div className="text-sm leading-5 text-gray-900">
-                                <Link to={team.id}>{i+1}</Link>
-                            </div>
-                        </td>
-
-                        <td className="px-6 py-4 border-b border-gray-200 whitespace-nowrap">
-                            <div className="text-sm leading-5 text-gray-900">
-                                {team?.name ? team?.name : '-'}
-                            </div>
-                        </td>
-
-                        <td className="px-6 py-4 border-b border-gray-200 whitespace-nowrap">
-                            <span className={`capitalize inline-flex px-2 text-xs font-semibold leading-5 ${industryColor(team.industry)} rounded-full`}>
-                                {team?.industry ? team.industry : '-'}
-                            </span>
-                        </td>
+                            <td className="px-6 py-4 border-b border-gray-200 whitespace-nowrap">
+                                    <span className={`capitalize inline-flex px-2 text-xs font-semibold leading-5 ${industryColor(team.industry)} rounded-full`}>
+                                        {team?.industry ? team.industry : '-'}
+                                    </span>
+                            </td>
 
 
-                        <td className="px-6 py-4 text-sm font-medium leading-5 text-right border-b border-gray-200 whitespace-nowrap">
-                            <Link to={team.id} className="text-indigo-600 hover:text-indigo-900 mr-2">View</Link>
-                            <button className="text-red-600 hover:text-red-900" data-id={team.id} onClick={handleDelete}>Delete</button>
-                        </td>
-                    </tr>
-                ))}
-            </>
-        </Table>}
-    </div>
+                            <td className="px-6 py-4 text-sm font-medium leading-5 text-right border-b border-gray-200 whitespace-nowrap">
+                                <Link to={team.id} className="text-indigo-600 hover:text-indigo-900 mr-2">View</Link>
+                                <button className="text-red-600 hover:text-red-900" data-id={team.id} onClick={handleDelete}>Delete</button>
+                            </td>
+                        </tr>
+                    ))}
+                </>
+            </Table>
+        </List>
   );
 }
 
