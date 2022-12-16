101b422cd268c446ec57ae6462a811f34c6ac148
Mario Henriquez <mariohenriquez@Marios-MacBook-Pro.local>
2022-12-16 14:27:38 -0500

    adding delete clients function on the clients list

--- src/components/clients/List.js
+++ src/components/clients/List.js
@@ -6,6 +6,7 @@ function ClientsList() {
     const baseUrl = 'http://localhost:3000/clients/';
     const [clients, setClients] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
+    const [currentDeleted, setCurrentDeleted] = useState('')
 
     useEffect(() => {
         fetch(`${baseUrl}`)
@@ -16,6 +17,43 @@ function ClientsList() {
             })
     }, []);
 
+    useEffect(() => {
+        let ignore = false;
+
+        const options = {
+            method: 'DELETE',
+            headers: {
+                'Content-type': 'application/json'
+            }
+        }
+
+        if (!ignore && currentDeleted !== '') {
+            fetch(`${baseUrl}/${currentDeleted}`, options)
+                .then(res => {
+                    if (res.ok) {
+                        setClients(clients.filter(i => i.id !== currentDeleted))
+                        setCurrentDeleted('')
+                    }
+                })
+                .catch(err => console.error(err))
+        }
+
+        return () => {
+            ignore = true
+        }
+        
+    }, [currentDeleted, clients]);
+
+
+    function handleDelete(e) {
+        e.preventDefault()
+
+        const { target } = e;
+        const { dataset } = target;
+        const { id:clientID } = dataset;
+
+        setCurrentDeleted(clientID)
+    }
 
     return (
         <List isLoading={isLoading} title="Client" linkTo="/clients/create">
@@ -49,7 +87,7 @@ function ClientsList() {
 
                         <td className="px-6 py-4 font-medium leading-5 text-center border-b border-gray-200 whitespace-nowrap">
                             <button className="text-indigo-600 hover:text-indigo-900 mr-2 text-sm">View</button>
-                            <button className="text-red-600 hover:text-red-900 text-sm">Delete</button>
+                            <button className="text-red-600 hover:text-red-900" data-id={client.id} onClick={handleDelete}>Delete</button>
                         </td>
 
                     </tr>
